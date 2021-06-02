ccef30bd92f6739e70d72420166d610203429f93
Hope Crystal <hope.orobosa.henry@gmail.com>
2021-06-02 09:10:44 +0100

    update to js

--- calculator-js/script.js
+++ calculator-js/script.js
@@ -13,6 +13,7 @@ class Calculator {
     }
 
     delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
 
     }
 
@@ -66,7 +67,10 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        if (this.operation != null) {
+        this.previousOperandTextElement.innerText = 
+            `${this.previousOperand} ${this.operation}`
+        }
     }
 
 }
@@ -99,5 +103,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => {
     calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
